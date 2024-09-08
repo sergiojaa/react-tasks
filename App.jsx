9a140f31c8c577f1f5c6eb9f06eebdf57b3cd02a
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-09-08 15:37:57 +0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
 
-import './App.css'
-import SimpleCounter from './components/SimpleCounter'
-import ToDoList from './components/ToDoList'
+// import DynamicGreeting from './components/FavoriteColorPicker'
+// import SimpleCounter from './components/SimpleCounter'
+// import ToDoList from './components/ToDoList'
+// import ToggleVisibilty from './components/ToggleVisibilty'
+
+import Rate from "./components/Rate"
 
 function App() {
   return(
-    <div>
-  <SimpleCounter/>
-  <ToDoList/>
+    <div  >
+      <Rate/>
     </div>
 
 
